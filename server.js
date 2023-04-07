ba5e846564238c6f8175957cf627a616512fa5a8
Alice2127 <aiokada2128@gmail.com>
2023-04-07 15:18:48 +0900

    CSS patched

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+// 使用するフレームワーク等の指定
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
 
+// 静的ファイル（画像、動画、CSSファイルなど）を提供するためのミドルウェアの作成
+app.use(express.static('public'));
+
+// ルーティングの定義
 app.get('/', (req, res) => {
   res.render('hello.ejs');
 });
 
-// サーバーを起動するコードを貼り付けてください
+// サーバーの起動
 app.listen(3000);
